90b2951792e0343a8234e2d0949e199a9efe8bb6
Floris Erich <erich@ai.iit.tsukuba.ac.jp>
2016-10-19 11:08:17 +0900

    Rename stream -> Stream

--- public/js/models/stream.js
+++ public/js/models/stream.js
@@ -1,36 +1,103 @@
-define(['./dragger'], function (Dragger) {
-    function stream(jsplumb, connectionHandler, stream) {
-        var dragger = null;
-        var m = new Mottle({ smartClicks:true });
+define([
+        '../util/dragger',
+        '../util/JSPlumbInstance'
+    ],
+    function(Dragger,
+             jsplumb)
+    {
 
-        m.on("#stream" + stream.id(), "tap", function(e) {
-            var targetState = !stream.menuVisible();
-            stream.program.hideAllMenus();
-            stream.menuVisible(targetState);
-            return true;
-        });
+    class Stream {
 
-        jsplumb.draggable(
-            $("#stream" + stream.id()),
-            {
-                start: function (params) {
-                    t = connectionHandler.transmitter;
-                    dragger = new Dragger(
-                        stream.x(),
-                        stream.y(),
-                        t.updateStream.bind(t),
-                        params.el
-                    );
-                },
-                drag: function (params) {
-                    dragger.dragMove(params.pos[0], params.pos[1]);
-                },
-                stop: function (params) {
-                    dragger.dragStop(params.pos[0], params.pos[1]);
+        constructor(streamModule, id, name, x, y, program) {
+            var self = this;
+            this.streamModule = streamModule;
+            this.program = program;
+
+            this.streamClass = "stream";
+
+            this.menuVisible = ko.observable(false);
+            this.id = ko.observable(id);
+            this.name = ko.observable(name);
+            this.x = ko.observable(x);
+            this.y = ko.observable(y);
+
+            this.xpx = ko.computed(() => this.x() + "px");
+            this.ypx = ko.computed(() => this.y() + "px");
+
+            this.addable = true;
+            this.edittable = false;
+            this.deletable = true;
+
+            this.in = ko.computed(function () {
+                return self.program.operations().filter(
+                    (operator) => operator.destination() == self.id()
+                );
+            }, this);
+
+            this.out = ko.computed(function () {
+                return self.program.operations().filter(
+                    (operator) => {
+                        if (operator["source"]) {
+                            return operator.source() == self.id();
+                        } else if (operator["sources"]) {
+                            return operator.sources().find(
+                                (source) => source === self.id()
+                            );
+                        }
+                    }
+                );
+            }, this);
+
+            this.deleteClicked = function(operator) {
+                jsplumb.detachAllConnections(
+                    $('#stream' + self.id())
+                );
+                self.streamModule.delete(self.id());
+                return true;
+            };
+        }
+
+        draw() {
+            var self = this;
+            var dragger = null;
+            var m = new Mottle({ smartClicks:true });
+
+            m.on($("#stream" + self.id() + " > div.stream-item"), "tap", function(e) {
+                if (e.ctrlKey) {
+                    jsplumb.addToDragSelection(el);
+                } else {
+                    var targetState = !self.menuVisible();
+                    self.program.hideAllMenus();
+                    self.menuVisible(targetState);
+                    return true;
                 }
-            }
-        );
+            });
+
+            jsplumb.draggable(
+                $("#stream" + this.id()),
+                {
+                    start: function (params) {
+                        dragger = new Dragger(
+                            self.x(),
+                            self.y(),
+                            self.update.bind(self),
+                            params.el
+                        );
+                    },
+                    drag: function (params) {
+                        dragger.dragMove(params.pos[0], params.pos[1]);
+                    },
+                    stop: function (params) {
+                        dragger.dragStop(params.pos[0], params.pos[1]);
+                    }
+                }
+            );
+        }
+
+        update() {
+            this.streamModule.update(this.id(), this);
+        }
     }
 
-    return stream;
+    return Stream;
 });
